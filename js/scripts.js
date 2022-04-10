c0eccd94e015bd35917f567523d80ed73818ec35
Sean Keane <seankeane93@gmail.com>
2022-04-10 12:52:41 -0700

    created else statements for calculating sleep hours

--- js/scripts.js
+++ js/scripts.js
@@ -38,9 +38,35 @@ const getSleepHours = day => {
     getSleepHours('Sunday');
   
   
-  console.log(getSleepHours('Monday'));
+  console.log(getSleepHours('Wednesday'));
   console.log(getActualSleepHours());
   
+  const getIdealSleepHours = () => {
+    let idealHours = 8;
+    return idealHours * 7;
+  };
+  
+  const calculateSleepDebt = () => {
+    const actualSleepHours =
+    getActualSleepHours();
+    const idealSleepHours =
+    getIdealSleepHours();
+  
+    if(actualSleepHours === idealSleepHours)
+    {
+      console.log('You are getting enough sleep');
+      else if (actualSleepHours > idealSleepHours) {
+        console.log("You've got more sleep than needed.");
+      }
+      else if (actualSleepHours < idealSleepHours){
+        console.log("Ypu should get some rest.")
+      }
+      else {
+        console.log("Error!  Check your code.")
+      }
+  };
+  
+  calculateSleepDebt();
   
   
   
