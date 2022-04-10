db798f5f72ceae9b3753753d4f79638923f73f79
Sean Keane <seankeane93@gmail.com>
2022-04-10 12:20:27 -0700

    add if/else statements for days of the week

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+const getSleepHours = day => {
+
+  if (day === 'Monday') {
+    return 8;
+  }
+  else if (day === 'Tuesday') {
+    return 7;
+  }
+  else if (day === 'Wednesday') {
+    return 8;
+  }
+  else if (day === 'Thursay') {
+    return 8;
+  }
+  else if (day === 'Friday') {
+    return 8;
+  }
+  else if (day === 'Saturday') {
+    return 8;
+  }
+  else if (day === 'Sunday') {
+    return 8;
+  }
+  else {
+    return 'Please enter a day of the week';
+  }
+  
+  const
+  
+  
+  };
